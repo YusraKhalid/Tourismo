838dd4f7d46ba015aa4fde7efd647bd04dd77ddc
Yusra Khalid <yusrakhalid@Zains-MacBook-Pro.local>
2020-03-08 15:56:43 +0500

    the same as before with trip as name

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -3,8 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
-import Trip from '../imports/ui/Task.js/index.js';
+//import Trip from '../imports/ui/Task.js';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
+  //render(<Trip />, document.getElementById('display-info'));
 });
